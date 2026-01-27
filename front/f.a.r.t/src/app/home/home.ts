9ba409d3bc0c31e942b3733e2044c0bc01edd507
Cristian <cristianpmason@icloud.com>
2026-01-26 20:39:47 -0500

    started nav component!
    -imported fonts
    -started nav component
    -

---
+++ front/f.a.r.t/src/app/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderNav } from '../header-nav/header-nav';
+
+@Component({
+  selector: 'app-home',
+  imports: [HeaderNav],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  
+}
