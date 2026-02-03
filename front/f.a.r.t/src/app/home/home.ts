8cdcf139a9e4a7ffc213973dbd18be589affc69e
Cristian <cristianpmason@icloud.com>
2026-02-02 23:08:13 -0500

    added news section with news template
    -imported test jpg
    -need to work on the news parameters for dynamic fetching

--- front/f.a.r.t/src/app/home/home.ts
+++ front/f.a.r.t/src/app/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Faq } from '../faq/faq';
 import { AboutSignUp } from '../about-sign-up/about-sign-up';
+import { NewsSection } from "../news-section/news-section";
 
 @Component({
   selector: 'app-home',
-  imports: [Faq, AboutSignUp],
+  imports: [Faq, AboutSignUp, NewsSection],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
