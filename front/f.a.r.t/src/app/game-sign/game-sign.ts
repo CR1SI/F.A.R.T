6fe2e8950805f80217447a56238a68df342d8d58
Cristian <cristianpmason@icloud.com>
2026-02-07 05:36:01 -0500

    update
    -added gameList fetching api connection and gameList shows accordingly!

--- front/f.a.r.t/src/app/game-sign/game-sign.ts
+++ front/f.a.r.t/src/app/game-sign/game-sign.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameList } from '../game-list';
 
 @Component({
   selector: 'app-game-sign',
@@ -6,35 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './game-sign.html',
   styleUrl: './game-sign.css',
 })
-export class GameSign {
-  //will call an api that should return a list of the games
-  public games: Game[] = [
-    {team1: {name: "AUR", logoUrl: "assets/logos/AUR.svg"}, team2: {name: "TKY", logoUrl: "assets/logos/TKY.svg"},scoreTeam1: 3, scoreTeam2: 0, gamePlayed: true, dateChosen: true, opponentsSelected: true, vodLink: "", date: "FRI, FEB 13 - 3:00 PM"},
-    {team1: {name: "AUR", logoUrl: "assets/logos/AUR.svg"}, team2: {name: "TKY", logoUrl: "assets/logos/TKY.svg"},scoreTeam1: 3, scoreTeam2: 0, gamePlayed: true, dateChosen: true, opponentsSelected: true, vodLink: "", date: "FRI, FEB 13 - 3:00 PM"},
-    {team1: {name: "AUR", logoUrl: "assets/logos/AUR.svg"}, team2: {name: "TKY", logoUrl: "assets/logos/TKY.svg"},scoreTeam1: 3, scoreTeam2: 0, gamePlayed: true, dateChosen: true, opponentsSelected: true, vodLink: "", date: "FRI, FEB 13 - 3:00 PM"},
-    {team1: {name: "AUR", logoUrl: "assets/logos/AUR.svg"}, team2: {name: "TKY", logoUrl: "assets/logos/TKY.svg"},scoreTeam1: 3, scoreTeam2: 0, gamePlayed: true, dateChosen: true, opponentsSelected: true, vodLink: "", date: "FRI, FEB 13 - 3:00 PM"},
-    {team1: {name: "AUR", logoUrl: "assets/logos/AUR.svg"}, team2: {name: "TKY", logoUrl: "assets/logos/TKY.svg"},scoreTeam1: 3, scoreTeam2: 0, gamePlayed: true, dateChosen: true, opponentsSelected: true, vodLink: "", date: "FRI, FEB 13 - 3:00 PM"},
-  ]
+export class GameSign implements OnInit{
+  games: any[] = [];
 
-}
+  constructor(private GameList: GameList) { }
 
-type Team = {
-  name: string,
-  logoUrl: string,
-  players?: string[],
-  losses?: number,
-  wins?: number,
-  points?: number,
-}
-
-type Game = {
-  team1: Team,
-  team2: Team,
-  scoreTeam1: number,
-  scoreTeam2: number,
-  gamePlayed: boolean,
-  dateChosen: boolean,
-  opponentsSelected: boolean,
-  vodLink: string,
-  date: string,
+  ngOnInit(): void {
+      this.GameList.getGames().subscribe(data => {
+        this.games = data;
+      });
+  }
 }
