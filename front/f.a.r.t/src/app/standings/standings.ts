e4518ba0bdd299a8817b3c25d883d21cca0a5a53
Cristian <cristianpmason@icloud.com>
2026-02-09 02:49:43 -0500

    update
    -updated about section
    -finished and made standings page
    -made api service for future api calls

--- front/f.a.r.t/src/app/standings/standings.ts
+++ front/f.a.r.t/src/app/standings/standings.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Api } from '../api';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-standings',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './standings.html',
   styleUrl: './standings.css',
 })
-export class Standings {
+export class Standings implements OnInit{
+  teams$!: Observable<any[]>;
 
+  constructor(private api: Api){}
+
+  sortByPoints = (a: any, b:any): number => {
+    const pointsA = a.points;
+    const pointsB = b.points;
+    if(pointsA < pointsB) {
+      return 1;
+    }
+    if (pointsA > pointsB){
+      return -1;
+    }
+    return 0;
+  }
+
+  ngOnInit(): void {
+      this.teams$ =this.api.getTeams().pipe(map(teamsArray => {
+        return teamsArray.sort(this.sortByPoints);
+      }));
+  }
 }
+
+
