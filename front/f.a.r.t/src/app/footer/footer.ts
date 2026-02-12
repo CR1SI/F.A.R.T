df96e7fdbf887e64921126501d98708c52ae1e42
Cristian <cristianpmason@icloud.com>
2026-02-12 06:02:53 -0500

    updated footer
    x and yt svg logos!

--- front/f.a.r.t/src/app/footer/footer.ts
+++ front/f.a.r.t/src/app/footer/footer.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Api } from '../api';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
